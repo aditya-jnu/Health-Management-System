4c1e3e8c9ac18d07f02c67787131652e605b792c
Your Name <rishabhyadav70070@gmail.com>
2025-01-11 15:29:40 +0530

    created new route

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -5,6 +5,7 @@ import Home from './components/Home';
 import Dashboard from './components/Dashboard';
 import ProtectedRoute from './components/ProtectedRoute';
 import Geolocation from './components/GeoLocation';
+import GetHospital from './components/GetHospital';
 
 function App() {
   return (
@@ -14,6 +15,7 @@ function App() {
         <Route path="/" element={<About />} />
         <Route path="/home" element={<Home />} />
          <Route path="/dashboard" element={<Dashboard />} />
+         <Route path="/getHospital" element={<GetHospital />} />
       </Routes>
     </Router>
   );
--- frontend/src/components/GetHospital.js
+++ frontend/src/components/GetHospital.js
@@ -1,11 +1,12 @@
+import NearByHosp from "./NearByHosp";
 import Navbar from "./Navbar";
-const Home = () => {
+const GetHospital = () => {
     return (
       <div className="min-h-screen bg-gray-100">
         <div><Navbar/></div>        
+        <div><NearByHosp/></div>
       </div>
     );
   };
   
-  export default Home;
-  
+  export default GetHospital;
