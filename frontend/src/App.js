db2155befe5af0e6492cafe18eccde77abefb432
Your Name <rishabhyadav70070@gmail.com>
2025-01-18 00:37:35 +0530

    Adding temp Contact Comp

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -7,6 +7,7 @@ import Geolocation from './components/GeoLocation';
 import Bookappointment from './components/Bookappointment';
 import Myappointment from './components/Myappointment';
 import Layout from './Layout';
+import Contact from './components/Contact/Contact';
 
 function App() {
   return (
@@ -21,6 +22,7 @@ function App() {
           <Route path="dashboard" element={<Dashboard />} />
           <Route path="bookappointment" element={<Bookappointment/>} />
           <Route path="myappointment" element={<Myappointment />} />
+          <Route path="contact" element={<Contact />} />
         </Route>
       </Routes>
     </Router>
